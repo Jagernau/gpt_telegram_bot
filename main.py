ba5a50c116fff595557a3adb7b381607f64f4a4d
jagernau <stepanenkov25@gmail.com>
2023-03-12 11:09:15 +0300

    поправил логику

--- main.py
+++ main.py
@@ -4,26 +4,30 @@ from utils import get_message, get_pic
 
 
 @bot.message_handler(content_types=['text'])
-def start(message: Message):
+def get_gpt_result(message: Message):
     """
     Owner verified.
     When the bot starts, 3 buttons are sent.
 
     """
     if str(message.from_user.id) == USER_ID and message.from_user.first_name == USER_NAME:
+        try:
 
-        if "Сгенерируй" or "Создай" or "Generate" in str(message.text) :
-            bot.send_photo(
-                    str(USER_ID),
-                    get_pic(str(message.text)),
-                    reply_to_message_id=message.message_id
-            )
+            if "Generate" in str(message.text) :
+                bot.send_photo(
+                        str(USER_ID),
+                        get_pic(str(message.text)),
+                        reply_to_message_id=message.message_id
+                )
 
 
-        else:
-            bot.send_message(
-                    str(USER_ID), 
-                    text=get_message(str(message.text))
-             )
+            else:
+                bot.send_message(
+                        str(USER_ID), 
+                        text=get_message(str(message.text))
+                 )
+        except:
+            bot.send_message(str(USER_ID), text="Что то пошло не так")
 
-bot.polling(none_stop=True, interval=0)
+
+bot.infinity_polling()
